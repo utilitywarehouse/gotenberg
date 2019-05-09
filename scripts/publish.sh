781339645b2d26422ee52b32a5af1aa55da1964f
Nicholas Jones <nicjones@utilitywarehouse.co.uk>
2019-05-09 11:31:42 +0100

    Push to quay.io

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,10 +4,8 @@ set -e
 
 GOLANG_VERSION="$1"
 VERSION="$2"
-DOCKER_USER="$3"
-DOCKER_PASSWORD="$4"
 
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+#docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
 VERSION="${VERSION//v}"
 SEMVER=( ${VERSION//./ } )   
@@ -18,17 +16,17 @@ if [ $VERSION_LENGTH -ne 3 ]; then
     exit 1
 fi
 
-docker build -t thecodingmachine/gotenberg:base -f build/base/Dockerfile .
+docker build -t quay.io/utilitywarehouse/gotenberg:base -f build/base/Dockerfile .
 docker build \
     --build-arg GOLANG_VERSION=${GOLANG_VERSION} \
     --build-arg VERSION=${VERSION} \
-    -t thecodingmachine/gotenberg:latest \
-    -t thecodingmachine/gotenberg:${SEMVER[0]} \
-    -t thecodingmachine/gotenberg:${SEMVER[0]}.${SEMVER[1]} \
-    -t thecodingmachine/gotenberg:${SEMVER[0]}.${SEMVER[1]}.${SEMVER[2]} \
+    -t quay.io/utilitywarehouse/gotenberg:latest \
+    -t quay.io/utilitywarehouse/gotenberg:${SEMVER[0]} \
+    -t quay.io/utilitywarehouse/gotenberg:${SEMVER[0]}.${SEMVER[1]} \
+    -t quay.io/utilitywarehouse/gotenberg:${SEMVER[0]}.${SEMVER[1]}.${SEMVER[2]} \
     -f build/package/Dockerfile .
 
-docker push "thecodingmachine/gotenberg:latest"
-docker push "thecodingmachine/gotenberg:${SEMVER[0]}"
-docker push "thecodingmachine/gotenberg:${SEMVER[0]}.${SEMVER[1]}"
-docker push "thecodingmachine/gotenberg:${SEMVER[0]}.${SEMVER[1]}.${SEMVER[2]}"
+docker push "quay.io/utilitywarehouse/gotenberg:latest"
+docker push "quay.io/utilitywarehouse/gotenberg:${SEMVER[0]}"
+docker push "quay.io/utilitywarehouse/gotenberg:${SEMVER[0]}.${SEMVER[1]}"
+docker push "quay.io/utilitywarehouse/gotenberg:${SEMVER[0]}.${SEMVER[1]}.${SEMVER[2]}"
